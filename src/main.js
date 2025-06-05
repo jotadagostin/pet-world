fca9c5ca1c5e1c8f3bdd996c21b88c409ae6defb
João Paulo Dagostin <107184368+jotadagostin@users.noreply.github.com>
2025-06-05 12:06:04 +0200

    setting new html and css

--- src/main.js
+++ src/main.js
@@ -4,17 +4,9 @@
 import "./libs/dayjs";
 
 //JS
-import "./modules/form/modal";
-import "./modules/form/add-schedule";
-import "./modules/form/remove-schedule";
 
 //css
 import "./styles/global.css";
-import "./styles/button.css";
-import "./styles/container-agenda.css";
-import "./styles/container-list.css";
-import "./styles/header.css";
-import "./styles/modal.css";
-
-import dayjs from "dayjs";
-console.log(dayjs().format("HH:mm"));
+import "./styles/form/index.css";
+import "./styles/main.css";
+import "./styles/section.css";
